77f53642d86dab7838173f83f633ccca7bafd329
lockdur <matvej.novik@gmail.com>
2022-12-30 18:02:51 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './scss/index.scss';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Here will be some text to test this widget</div>;
 }
 
 export default App;
